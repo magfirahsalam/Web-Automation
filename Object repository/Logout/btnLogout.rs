4e1cba5ff3bfe240e759a2f446fd0b8f319454a4
Magfirah Salam <43084693+magfirahsalam@users.noreply.github.com>
2023-11-07 14:21:26 +0700

    Add files via upload

---
+++ Object repository/Logout/btnLogout.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btnLogout</name>
+   <tag></tag>
+   <elementGuidId>2b7ec533-e0d5-405f-bc49-d7266084685a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//button[@class='btn btn-link text-decoration-none']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//button[@class='btn btn-link text-decoration-none']</value>
+      <webElementGuid>92ad1a35-5790-41ae-a22a-d343df26e191</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object repository/Logout/btnProfile.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btnProfile</name>
+   <tag></tag>
+   <elementGuidId>1d17736f-415c-4048-9ee1-1fd0658a634d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//li[@class='nav-item dropdown fs-5 d-none d-lg-block']//i[@class='bi bi-person me-4 me-lg-0']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//li[@class='nav-item dropdown fs-5 d-none d-lg-block']//i[@class='bi bi-person me-4 me-lg-0']</value>
+      <webElementGuid>c06a2ef5-4522-48e0-9bb9-6564d5902235</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
